2c39003a8e67c29080968627160452ba52d19c48
Ahmet Hakan Tulgaç <hakantulgac@gmail.com>
2024-03-17 17:35:25 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Bitirme Projesi - Futbol Analizi - Ui
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
